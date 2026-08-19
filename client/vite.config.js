5090aae0a9d498eea1f19bc0fa8150eb59548a96
tanskie <carlangelotan@gmail.com>
2026-08-19 16:38:08 +0800

    remove 127.0.011

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: process.env.API_TARGET || 'http://127.0.0.1:3000',
+        target: process.env.API_TARGET,
         changeOrigin: true,
       },
     },
