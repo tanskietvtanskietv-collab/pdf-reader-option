a20fc6914dc81a6fa972a05e1972712f2b507829
tanskie <carlangelotan@gmail.com>
2026-08-19 16:50:43 +0800

    commit vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: process.env.API_TARGET,
+        target: process.env.VITE_API_TARGET,
         changeOrigin: true,
       },
     },
